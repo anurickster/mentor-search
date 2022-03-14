98c412d3a2e6ab189d5b4803351899827416d434
Anurag Singh <anuragfrenk.com>
2022-03-15 00:09:52 +0530

    qq

--- src/Components/Aboutpage/Aboutpage.js
+++ src/Components/Aboutpage/Aboutpage.js
@@ -4,10 +4,6 @@ const Aboutpage = () => {
   return (
     <div>
       <h1>Aboutpage</h1>
-      <h2>change made by ashu</h2>
-      <h3>New change made by ashu</h3>
-      <h4>this change made by anurag</h4>
-      <h5>new change</h5>
     </div>
   );
 };
