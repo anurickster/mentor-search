20e992a30ac703d7edf23344aca4ad9e7c43ccbc
Ashwini Rathod <ashwinibaburathod@gmail.com>
2022-03-14 23:52:36 +0530

    ashu

--- src/Components/Aboutpage/Aboutpage.js
+++ src/Components/Aboutpage/Aboutpage.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const Aboutpage = () => {
   return (
     <div>
       <h1>Aboutpage</h1>
-      <h2>New Change 7</h2>
+      <h2>change by ashu</h2>
     </div>
   );
 };
