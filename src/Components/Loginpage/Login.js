6ddb4a25d5642a13bcdb2539a070ec7b5854c7fb
Anurag Singh <anuragfrenk.com>
2022-03-15 21:30:13 +0530

    updated

--- src/Components/Loginpage/Login.js
+++ src/Components/Loginpage/Login.js
@@ -5,9 +5,6 @@ function Login() {
     <div className='Login'>
       <header className='App-header'>
         <h3> Login Page </h3>
-        <h1>bhhfhgghjggjhjy</h1>
-        <hgroup>hhhgyg</hgroup>
-        <h1>uhuguguygy</h1>
         <a href='http://localhost:3000/homepage'>
           <button className='btn btn-primary'>Login</button>
         </a>
