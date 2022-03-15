c6e4b481743fe03fe6ffefbd193f9e822c38a0ff
Anurag Singh <anuragfrenk.com>
2022-03-15 20:19:41 +0530

    update

--- src/Components/Addmentor/Addmentor.js
+++ src/Components/Addmentor/Addmentor.js
@@ -1,18 +1,23 @@
-import axios from "axios";
-import React from "react";
-import Navigationbar from "../Navigationbar/Navigationbar";
-import "./module.Addmentor.css";
+import axios from 'axios';
+import React, { useState } from 'react';
+import Navigationbar from '../Navigationbar/Navigationbar';
+import './module.Addmentor.css';
 
 const Addmentor = () => {
   const onAddMentor = () => {
-    axios.post("http://localhost:9000/posts", {
-      mentorName: "Ashwini Rathod",
-      imgUrl: "https://miro.medium.com/max/895/0*l0QEGkMny8Ifq5pQ.png",
-      yearExperience: 2,
-      monthExperience: 10,
-      mentorSkills: ["C++", "C", "Node.js", "React.js"],
+    axios.post('http://localhost:9000/posts', {
+      mentorName: mName,
+      imgUrl: 'https://miro.medium.com/max/895/0*l0QEGkMny8Ifq5pQ.png',
+      yearExperience: mYrExp,
+      monthExperience: mMnExp,
+      mentorSkills: ['C++', 'C', 'Node.js', 'React.js'],
     });
   };
+
+  const [mName, setMName] = useState('');
+  const [mYrExp, setMYrExp] = useState('');
+  const [mMnExp, setMMnExp] = useState('');
+  const [mImgUrl, setMImgUrl] = useState([]);
 
   return (
     <>
@@ -21,17 +26,17 @@ const Addmentor = () => {
         <label htmlFor='mentorName' name='mentorName'>
           Enter Name
         </label>
-        <input type='text' />
+        <input type='text' onChange={(e) => setMName(e.target.value)} />
         <label htmlFor='mentorName' name='mentorName'>
           Enter Experience in years
         </label>
-        <input type='text' />
+        <input type='text' onChange={(e) => setMYrExp(e.target.value)} />
         <label htmlFor='mentorName' name='mentorName'>
           Enter Experience in months
         </label>
-        <input type='text' />
+        <input type='text' onChange={(e) => setMMnExp(e.target.value)} />
         <label htmlFor='imageUrl'>Enter Image URL</label>
-        <input type='text' />
+        <input type='text' onChange={(e) => setMImgUrl(e.target.value)} />
         <button type='button' onClick={onAddMentor} className='btn btn-primary'>
           Add Mentor
         </button>
