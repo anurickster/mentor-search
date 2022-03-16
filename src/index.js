8c9d241865651c9ef21021529264eec6bb98f422
Ashwini Rathod <ashwinibaburathod@gmail.com>
2022-03-16 10:27:46 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -1,34 +1,26 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import postReducer from './store/post-reducer';
-import thunkMiddleware from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import postReducer from "./store/post-reducer";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 // create a middleware
 
 const datetime = (store) => (next) => (action) => {
-  console.log('dispatched at ', new Date());
+  console.log("dispatched at ", new Date());
   let result = next(action);
   // console.log('next state', store.getState())
   return result;
 };
 
 const logger = (store) => (next) => (action) => {
-  console.log('dispatching', action);
+  console.log("dispatching", action);
   let result = next(action);
-  console.log('next state', store.getState());
+  console.log("next state", store.getState());
   return result;
 };
 
@@ -47,6 +39,5 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
->>>>>>> 2f8f9a2354de5ffdc1342e30a41ae9c0f87d514c
