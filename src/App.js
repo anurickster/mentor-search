370eef5403dd5d7cdd796126304001ded77b3932
Anurag Singh <anuragfrenk.com>
2022-03-16 12:22:21 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './Components/Homepage/Homepage';
 import Addmentor from './Components/Addmentor/Addmentor';
@@ -8,11 +7,6 @@ import Login from './Components/Loginpage/Login';
 import Contactpage from './Components/Contactpage/Contactpage';
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Homepage from './Components/Homepage/Homepage';
-import Addmentor from './Components/Addmentor/Addmentor';
-import Aboutpage from './Components/Aboutpage/Aboutpage';
-import Login from './Components/Loginpage/Login';
 
 function App() {
   return (
