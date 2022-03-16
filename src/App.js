2a8a87c8a68875a27585900e45633b7625702416
Anurag Singh <anuragfrenk.com>
2022-03-16 12:00:34 +0530

    Fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Homepage from "./Components/Homepage/Homepage";
-import Addmentor from "./Components/Addmentor/Addmentor";
-import Aboutpage from "./Components/Aboutpage/Aboutpage";
-import Login from "./Components/Loginpage/Login";
-import Contactpage from "./Components/Contactpage/Contactpage";
-=======
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Homepage from './Components/Homepage/Homepage';
+import Addmentor from './Components/Addmentor/Addmentor';
+import Aboutpage from './Components/Aboutpage/Aboutpage';
+import Login from './Components/Loginpage/Login';
+import Contactpage from './Components/Contactpage/Contactpage';
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -16,7 +13,6 @@ import Homepage from './Components/Homepage/Homepage';
 import Addmentor from './Components/Addmentor/Addmentor';
 import Aboutpage from './Components/Aboutpage/Aboutpage';
 import Login from './Components/Loginpage/Login';
->>>>>>> 2f8f9a2354de5ffdc1342e30a41ae9c0f87d514c
 
 function App() {
   return (
