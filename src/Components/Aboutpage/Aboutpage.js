1d855e958ace262f20f346b16a1584275882e9d6
Ashwini Rathod <ashwinibaburathod@gmail.com>
2022-03-20 19:11:06 +0530

    Changes Done

--- src/Components/Aboutpage/Aboutpage.js
+++ src/Components/Aboutpage/Aboutpage.js
@@ -18,7 +18,7 @@ const Aboutpage = () => {
                     Grow Your Knowledge with
                     <strong className='brand-name ps-2'>FindMentor.com </strong>
                   </h1>
-                  <h2 className='my-3'>
+                  <h2 className='my-3 '>
                     FindMentor Hiring will help you connect with the skills of
                     developers in our community who are actively looking for
                     work.
@@ -29,7 +29,7 @@ const Aboutpage = () => {
                     </a>
                   </div>
                 </div>
-                <div className='col-lg-6 order-1 order-lg-2  ps-5 header-img'>
+                <div className='col-lg-6 order-1 order-lg-2  ps-5 header-img pb-5'>
                   <img
                     src={web}
                     className='img-fluid animated'
