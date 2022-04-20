9796836ae5f2e77c5fbd25f352b26840c7a102ca
Mohit Gandhewar <mohit.gandhewar@glogic.co.in>
2022-04-20 11:20:10 +0530

    view courses and add courses reday with db.json

--- Client/src/Components/AddCoursepage/AddCoursepage.js
+++ Client/src/Components/AddCoursepage/AddCoursepage.js
@@ -1,36 +1,161 @@
-import React from 'react';
-import './module.AddCoursepage.css';
-import Navigationbar from '../Navigationbar/Navigationbar';
-import Footer from '../Footerpage/Footer';
+import React from "react";
+import Navigationbar from "../Navigationbar/Navigationbar";
+import Footer from "../Footerpage/Footer";
+import { useEffect } from "react";
+import axios from "axios";
+import Learning from "./Learning.png";
+import { useNavigate, useParams } from "react-router-dom";
+import MultipleInput from "./MultipleInput";
+import "./module.AddCoursepage.css";
 
 const AddCoursepage = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const [mentorsDetails, setMentorsDetails] = React.useState({
+    id: localStorage.getItem("userid"),
+    mname: "",
+    experience: "",
+    skills: [],
+    courses: [],
+    linkedin: "",
+    github: "",
+  });
+  console.log(mentorsDetails);
+
+  const [addedskills, setAddedSkills] = React.useState([]);
+  console.log(addedskills);
+  const [addedCourses, setAddedCourses] = React.useState([]);
+
+  useEffect(() => {
+    if (id) {
+      loadMentor();
+      addedskills.push(...mentorsDetails.skills);
+      setAddedSkills(mentorsDetails.skills);
+      setMentorsDetails({ mname: localStorage.getItem("userName") });
+    }
+  }, []);
+  console.log(mentorsDetails.mname);
+  console.log(addedskills);
+
+  const changeHandler = (e) => {
+    setMentorsDetails({ ...mentorsDetails, [e.target.name]: e.target.value });
+  };
+
+  const loadMentor = async () => {
+    const result = await axios.get(`http://localhost:9000/mentors/${id}`);
+    console.log(result.data);
+    setMentorsDetails(result.data);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    return (
+      mentorsDetails.skills.push(...addedskills),
+      mentorsDetails.courses.push(...addedCourses),
+      await axios
+        .post("http://localhost:9000/mentors", mentorsDetails)
+
+        .then((res) => {
+          console.log(res.data);
+          if (res.status === 200) navigate("/home");
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    );
+  };
+
+  const updateMentor = async (index, e) => {
+    e.preventDefault();
+    return (
+      mentorsDetails.skills.push(...addedskills),
+      mentorsDetails.courses.push(...addedCourses),
+      await axios
+        .put(`http://localhost:9000/mentors/${index}`, mentorsDetails)
+        .then((res) => {
+          console.log(res.data);
+          if (res.status === 200) navigate("/home");
+          id = null;
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    );
+  };
+
   return (
     <>
       <Navigationbar />
-      <div className='main-profile'>
-        <h1>Hi Mohit</h1>
-        <h3># Things to keep in Mind</h3>
-        <ul>
-          <li>Pull The code</li>
-          <li>
-            you have to make a page for Add Courses by Mentor in this file
-          </li>
-          <li>
-            for styling use{' '}
-            <b>./Components/AddCoursepage/moudule.AddCoursepage.css</b>
-          </li>
-          <li>
-            this page is for student role when student come he/she can apply by
-            filling details as per the requirement
-          </li>
-        </ul>
-        <p className='example-of-usage'>
-          if you are facing any issues please let me assist you.
-        </p>
-        <br />
-        <br />
-        <h4>Please See Reffrence below</h4>
-        <br />
+
+      <div className="Services">
+        <div>
+          <img src={Learning} alt="Learning" className="MentorFormImage" />
+        </div>
+        <div>
+          <form className="Mentor_Form">
+            <h2>
+              Join Us for creating a better <br></br>Learning Experience{" "}
+            </h2>
+            <div>
+              <label htmlFor="mname"></label>
+              <input
+                type="text"
+                name="mname"
+                placeholder="Mentor Name"
+                value={mentorsDetails.mname || ""}
+                onChange={changeHandler}
+              />
+            </div>
+            <label htmlFor="experience"></label>
+            <div>
+              <input
+                type="number"
+                name="experience"
+                placeholder="Experience"
+                value={mentorsDetails.experience || ""}
+                onChange={changeHandler}
+              />
+            </div>
+            <MultipleInput
+              Array={addedskills}
+              SetArray={setAddedSkills}
+              placeholder={"Add Skills"}
+            />
+            <MultipleInput
+              Array={addedCourses}
+              SetArray={setAddedCourses}
+              placeholder={"Add Courses"}
+            />
+            <label htmlFor="linkdin"></label>
+            <div>
+              <input
+                type="text"
+                name="linkedin"
+                placeholder="Linkedin profile link"
+                value={mentorsDetails.linkedin || ""}
+                onChange={changeHandler}
+              />
+            </div>
+            <label htmlFor="github"></label>
+            <div>
+              <input
+                type="text"
+                name="github"
+                placeholder="Github profile Link"
+                value={mentorsDetails.github || ""}
+                onChange={changeHandler}
+              />
+            </div>
+
+            <button
+              className="Submit BTN"
+              onClick={id ? (e) => updateMentor(id, e) : (e) => handleSubmit(e)}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
       <Footer />
     </>
--- Client/src/Components/ViewCoursespage/ViewCoursespage.js
+++ Client/src/Components/ViewCoursespage/ViewCoursespage.js
@@ -1,37 +1,175 @@
-import React from 'react';
-import './module.ViewCoursespage.css';
-import Navigationbar from '../Navigationbar/Navigationbar';
-import Footer from '../Footerpage/Footer';
+import React from "react";
+import { useEffect } from "react";
+
+import "./module.ViewCoursespage.css";
+import Navigationbar from "../Navigationbar/Navigationbar";
+import Footer from "../Footerpage/Footer";
+import axios from "axios";
+import Img1 from "../Images/1.jpg";
+import Img2 from "../Images/2.jpg";
+import Img3 from "../Images/3.jpg";
+import Img4 from "../Images/4.jpg";
+import Img5 from "../Images/5.jpg";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Rating from "@mui/material/Rating";
+import { Link } from "react-router-dom";
 
 const ViewCoursespage = () => {
+  const [mentors, setMentors] = React.useState([""]);
+  const ImgArray = [Img1, Img2, Img3, Img4, Img5];
+  const [moreOptions, setMoreOptions] = React.useState(0); //for more options
+  const [usertype, setUsertype] = React.useState("");
+  const [userId, setUserId] = React.useState("");
+
+  useEffect(() => {
+    // if (localStorage.getItem("usertype")) {
+    // setUsertype(localStorage.getItem("usertype"));
+    // setUserId(localStorage.getItem("userid"));
+    axios
+      .get("http://localhost:9000/mentors")
+      .then((res) => {
+        console.log(res.data);
+        setMentors(res.data);
+        console.log(mentors);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    // } else {
+    // window.location.href = "/";
+    // }
+  }, [mentors.length]);
+
+  const ShowMoreOptions = (event, id) => {
+    event.preventDefault();
+    if (moreOptions === id) {
+      setMoreOptions(null);
+    } else {
+      setMoreOptions(id);
+    }
+  };
+
+  const DeleteMentor = async (event, id) => {
+    event.preventDefault();
+    await axios.delete(`http://localhost:9000/mentors/${id}`);
+
+    setMentors(mentors.filter((mentor) => mentor.id !== id));
+  };
+  // console.log(userId);
+
   return (
     <>
       <Navigationbar />
-      <div className='main-profile'>
-        <h1>Hi Mohit</h1>
-        <h3># Things to keep in Mind</h3>
-        <ul>
-          <li>Pull The code</li>
-          <li>
-            you have to make a page for View Courses by Viewer in this file
-          </li>
-          <li>
-            for styling use{' '}
-            <b>./Components/ViewCoursespage/moudule.ViewCoursespage.css</b>
-          </li>
-          <li>
-            this page is for student role when student come he/she can apply by
-            filling details as per the requirement
-          </li>
-        </ul>
-        <p className='example-of-usage'>
-          if you are facing any issues please let me assist you.
-        </p>
-        <br />
-        <br />
-        <h4>Please See Reffrence below</h4>
-        <br />
+      <div className="Home">
+        <div className="Cards_Container">
+          {mentors ? (
+            mentors.map((mentor, i) => {
+              // console.log(mentor.id);
+              return (
+                <div key={i} className="MentorsCard">
+                  <div className="leftSection">
+                    <h3 className="Mentor__NameSec">
+                      <p> {mentor.mname}</p>
+                      <span onBlur={() => setMoreOptions(null)}>
+                        {mentor.linkedin ? (
+                          <a
+                            href={mentor.linkedin}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <LinkedInIcon className="Linkedin__Icon" />
+                          </a>
+                        ) : null}
+                        {mentor.github ? (
+                          <a
+                            href={mentor.github}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <GitHubIcon className="Github__Icon" />
+                          </a>
+                        ) : null}
+                        {usertype === "mentor" && userId == mentor.id ? (
+                          <>
+                            <MoreVertIcon
+                              className="More__Icon"
+                              onClick={(e) => ShowMoreOptions(e, mentor.id)}
+                            />
+                            <ul
+                              className={
+                                moreOptions === mentor.id
+                                  ? "Show MoreOptions"
+                                  : "MoreOptions"
+                              }
+                            >
+                              <Link to={`/mentor/${mentor.id}`} exact="true">
+                                <li>Edit</li>
+                              </Link>
+                              <li onClick={(e) => DeleteMentor(e, mentor.id)}>
+                                Delete
+                              </li>
+                            </ul>
+                          </>
+                        ) : null}
+                      </span>
+                    </h3>
+                    <div className="Mentors__Rating">
+                      Rating:
+                      <Rating
+                        className="Rating"
+                        name="half-rating"
+                        defaultValue={Math.ceil(Math.random() * 5)}
+                        precision={0.2}
+                        size="small"
+                      />
+                      <p>{} </p>
+                    </div>
+                    <p> Experience : {mentor.yearExperience} Years</p>
+
+                    <p>Skills:</p>
+                    <ul className="mentorskills">
+                      {mentor.skills
+                        ? mentor.skills.map((skill, id) => {
+                            return (
+                              <li key={id} className="skills">
+                                {skill}
+                              </li>
+                            );
+                          })
+                        : null}
+                    </ul>
+                    <p>Courses:</p>
+                    <ul className="mentorCourses">
+                      {mentor.courses
+                        ? mentor.courses.map((course, id) => {
+                            return (
+                              <li className="Course" key={id}>
+                                {course}
+                              </li>
+                            );
+                          })
+                        : null}
+                    </ul>
+                  </div>
+                  <div className="rightSection">
+                    <img
+                      src={ImgArray[Math.floor(Math.random() * 5)]}
+                      alt="mentorCourse"
+                    />
+                    <p>inventore laudantium exercitationem, magni ab!</p>
+                    <button className="BTN ViewDetails ">View Details</button>
+                  </div>
+                </div>
+              );
+            })
+          ) : (
+            <h3>No Mentors Present add some </h3>
+          )}
+        </div>
       </div>
+
       <Footer />
     </>
   );
