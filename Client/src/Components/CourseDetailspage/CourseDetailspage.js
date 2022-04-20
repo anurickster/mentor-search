ddddbfca037233aa234c4e7c56999a9665b4b998
Mohit Gandhewar <mohit.gandhewar@glogic.co.in>
2022-04-21 04:00:40 +0530

    coursedetails page Ui working with real data

--- Client/src/Components/CourseDetailspage/CourseDetailspage.js
+++ Client/src/Components/CourseDetailspage/CourseDetailspage.js
@@ -3,7 +3,7 @@ import "./module.CourseDetailspage.css";
 import Navigationbar from "../Navigationbar/Navigationbar";
 import Footer from "../Footerpage/Footer";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Rating from "@mui/material/Rating";
 
 const CourseDetailspage = () => {
@@ -26,76 +26,78 @@ const CourseDetailspage = () => {
   return (
     <>
       <Navigationbar />
+
       <div className="Course_details_container">
         <div className="Course_details_container_left">
-          <div className="course_Heading">
+          <div className="courseDetails_Heading">
             <h1>{course.course_title}</h1>
-            <Rating
-              className="Rating"
-              name="half-rating"
-              value={Math.ceil(Math.random() * 5)}
-              precision={0.2}
-              size="large"
-              color="secondary"
-              controlled="true"
-            />
+            <div className="flexRow">
+              <Rating
+                className="Rating"
+                name="half-rating"
+                value={Math.ceil(Math.random() * 5)}
+                precision={0.2}
+                size="large"
+                color="secondary"
+                controlled="true"
+              />
+              <p className="Reviews">
+                ( {Math.ceil(Math.random() * 500 + 500)} Reviews )
+              </p>
+            </div>
+            <p className="Mentor_Name">
+              Created by:{" "}
+              <Link className="ViewDetails " to={`/mentorprofile`} exact="true">
+                {course.mentor_name}
+              </Link>
+            </p>
           </div>
-          <div>{course.lectures} </div>
-          <div>{course.course_duration}</div>
+          {/* <div>{course.lectures} </div>
+          <div>{course.course_duration}</div> */}
           {/* <div>{course.students} students enrolled already</div> */}
           <div className="no_of_students">
             {Math.ceil(Math.random() * 2000 + 2000)} students enrolled already
           </div>
           <div>{course.discription}</div>
-          <ul>
+          <p>Course is for</p>
+          <ul className="Course_Is_for flexRow">
             {course.level
               ? course.level.map((item, i) => {
                   return <li key={i}>{item}</li>;
                 })
               : ""}
           </ul>
-          <h3>What you will learn</h3>
-          <div className="course_content_container">
-            <p> {course.course_duration} Hours of Videos</p>
-            <p> {course.lectures} Lectures</p>
-            <p> And Assignments on each topic</p>
-
-            <ul>
-              {course.course_content
-                ? course.course_content.map((item, i) => {
-                    return <li key={i}>{item}</li>;
-                  })
-                : ""}
-            </ul>
-          </div>
         </div>
         <div className="Course_details_container_right">
           <div className="Course_details_container_right_Img">
-            <img src={course.image} alt="" />
+            <h4>{course.price}</h4>
           </div>
           <div className="Course_details_container_right_details"></div>
         </div>
       </div>
 
-      {/* <div className="main-profile">
-        <h1>Hi Mohit</h1>
-        <h3># Things to keep in Mind</h3>
-        <ul>
-          <li>Pull The code</li>
-          <li>you have to make a page for Course Details in this file</li>
-          <li>
-            for styling use{" "}
-            <b>./Components/CourseDetailspage/moudule.CourseDetailspage.css</b>
-          </li>
-          <li>
-            this page is for student role when student come he/she can apply by
-            filling details as per the requirement
-          </li>
+      <div className="course_Topics_container">
+        <h3 className="sec_Heading">What you will learn</h3>
+        <ul className="course_content_container flexRow">
+          <li> {course.course_duration} Hours of Videos</li>
+          <li> {course.lectures} Lectures</li>
+          <li> Assignments on each topic</li>
         </ul>
-        <p className="example-of-usage">
-          if you are facing any issues please let me assist you.
-        </p>
-      </div> */}
+        <div>
+          <h3 className="sec_Heading"> Topics covered</h3>
+          <ul>
+            {course.course_content
+              ? course.course_content.map((item, i) => {
+                  return (
+                    <li className="Topics" key={i}>
+                      {item}
+                    </li>
+                  );
+                })
+              : ""}
+          </ul>
+        </div>
+      </div>
 
       <Footer />
     </>
--- Client/src/Components/ViewCoursespage/ViewCoursespage.js
+++ Client/src/Components/ViewCoursespage/ViewCoursespage.js
@@ -28,16 +28,16 @@ const ViewCoursespage = () => {
     // if (localStorage.getItem("usertype")) {
     // setUsertype(localStorage.getItem("usertype"));
     // setUserId(localStorage.getItem("userid"));
-    axios
-      .get("http://localhost:7000/mentors")
-      .then((res) => {
-        console.log(res.data);
-        setMentors(res.data);
-        console.log(mentors);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    // axios
+    //   .get("http://localhost:7000/mentors")
+    //   .then((res) => {
+    //     console.log(res.data);
+    //     setMentors(res.data);
+    //     console.log(mentors);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
     axios
       .get("http://localhost:9000/courses")
       .then((res) => {
@@ -86,111 +86,6 @@ const ViewCoursespage = () => {
     <>
       <Navigationbar />
       <div className="Home">
-        <div className="Cards_Container">
-          {/* {mentors ? (
-            mentors.map((mentor, i) => {
-              // console.log(mentor.id);
-              return (
-                <div key={i} className="MentorsCard">
-                  <div className="leftSection">
-                    <h3 className="Mentor__NameSec">
-                      <p> {mentor.mname}</p>
-                      <span onBlur={() => setMoreOptions(null)}>
-                        {mentor.linkedin ? (
-                          <a
-                            href={mentor.linkedin}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            <LinkedInIcon className="Linkedin__Icon" />
-                          </a>
-                        ) : null}
-                        {mentor.github ? (
-                          <a
-                            href={mentor.github}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            <GitHubIcon className="Github__Icon" />
-                          </a>
-                        ) : null}
-                        {usertype === "mentor" && userId == mentor.id ? (
-                          <>
-                            <MoreVertIcon
-                              className="More__Icon"
-                              onClick={(e) => ShowMoreOptions(e, mentor.id)}
-                            />
-                            <ul
-                              className={
-                                moreOptions === mentor.id
-                                  ? "Show MoreOptions"
-                                  : "MoreOptions"
-                              }
-                            >
-                              <Link to={`/mentor/${mentor.id}`} exact="true">
-                                <li>Edit</li>
-                              </Link>
-                              <li onClick={(e) => DeleteMentor(e, mentor.id)}>
-                                Delete
-                              </li>
-                            </ul>
-                          </>
-                        ) : null}
-                      </span>
-                    </h3>
-                    <div className="Mentors__Rating">
-                      Rating:
-                      <Rating
-                        className="Rating"
-                        name="half-rating"
-                        defaultValue={Math.ceil(Math.random() * 5)}
-                        precision={0.2}
-                        size="small"
-                      />
-                      <p>{} </p>
-                    </div>
-                    <p> Experience : {mentor.yearExperience} Years</p>
-
-                    <p>Skills:</p>
-                    <ul className="mentorskills">
-                      {mentor.skills
-                        ? mentor.skills.map((skill, id) => {
-                            return (
-                              <li key={id} className="skills">
-                                {skill}
-                              </li>
-                            );
-                          })
-                        : null}
-                    </ul>
-                    <p>Courses:</p>
-                    <ul className="mentorCourses">
-                      {mentor.courses
-                        ? mentor.courses.map((course, id) => {
-                            return (
-                              <li className="Course" key={id}>
-                                {course}
-                              </li>
-                            );
-                          })
-                        : null}
-                    </ul>
-                  </div>
-                  <div className="rightSection">
-                    <img
-                      src={ImgArray[Math.floor(Math.random() * 5)]}
-                      alt="mentorCourse"
-                    />
-                    <p>inventore laudantium exercitationem, magni ab!</p>
-                    <button className="BTN ViewDetails ">View Details</button>
-                  </div>
-                </div>
-              );
-            })
-          ) : (
-            <h3>No Mentors Present add some </h3>
-          )} */}
-        </div>
         <div className="Courses_Container">
           {coursesList ? (
             coursesList.map((course, i) => {
@@ -251,10 +146,11 @@ const ViewCoursespage = () => {
                       <Rating
                         className="Rating"
                         name="half-rating"
-                        defaultValue={Math.ceil(Math.random() * 5)}
+                        value={Math.ceil(Math.random() * 5)}
                         precision={0.2}
                         size="small"
                         color="secondary"
+                        controlled="false"
                       />
                       <p>{} </p>
                     </div>
